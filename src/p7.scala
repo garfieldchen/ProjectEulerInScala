15f335c38d6ff136fb7628a59d40afda0f877875
garfieldchen <garfieldchen@me.com>
2014-07-13 23:44:59 +0800

    add p7, prim check reduce fast

---
+++ src/p7.scala
@@ -0,0 +1,40 @@
+object PE {
+  def main(args: Array[String]) {
+    println(findn(6))
+    println(findn(10001))
+  }
+
+  def findn(n: Int): Long = {
+    var i = 5L
+    var count = n - 2
+    while( count > 0) {
+      //println(s"$i => ${isPrim(i)}")
+      if (isPrim(i))
+        count -= 1
+
+      i += 2
+    }
+
+    i - 2
+  }
+
+
+  def isPrim(n: Long) = n match{
+    case 1 => false
+    case 2 => true
+    case 3 => true
+    case _ if (n % 3==0 || n % 2==0) => false
+    case _ =>
+      var top = n / 3
+      var i = 5
+      var result = true
+      while ( result && i < top) {
+        result = n % i != 0
+        top = n / i
+        i += 2
+      }
+
+      result
+  }
+}
+
